0abe47677e94c7c0d26517ef8e96c7007d8e35d2
Flint Louis <flintlouis@Flints-MacBook-Pro.local>
2021-03-04 16:32:09 +0100

    Example added

---
+++ example.py
@@ -0,0 +1,11 @@
+# import pandas as pd
+# from functions import dropCol
+
+# file_path = ""
+
+# df = pd.read_csv(file_path)
+# print(df.head()) # Prints first 5 datapoints
+# newdf = dropCol(df, ['NameCol1', 'NameCol2', 'NameCol3'])
+# #or
+# newdf = dropCol(df, 'NameCol1')
+# print(newdf.head())
